0f3b292cc01c13042fa01c7ec847497380938648
denglei <804956748@qq.com>
2020-08-06 22:24:21 +0800

    端口改为80，更改配置文件

--- webapp/src/main/java/com/pomo/wxtimeblock/WxtimeblockApplication.java
+++ webapp/src/main/java/com/pomo/wxtimeblock/WxtimeblockApplication.java
@@ -2,12 +2,20 @@ package com.pomo.wxtimeblock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class WxtimeblockApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WxtimeblockApplication.class, args);
-	}
+    @GetMapping("/")
+    public String index() {
+        return "redirect:swagger-ui.html";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxtimeblockApplication.class, args);
+    }
 
 }
