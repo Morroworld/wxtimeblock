66542a8eee7bb53a38512f61e9743be730a43357
denglei <804956748@qq.com>
2020-08-07 21:46:41 +0800

    appid and appsecret

--- webapp/src/main/java/com/pomo/wxtimeblock/constants/WxConstants.java
+++ webapp/src/main/java/com/pomo/wxtimeblock/constants/WxConstants.java
@@ -7,9 +7,9 @@ package com.pomo.wxtimeblock.constants;
  */
 public class WxConstants {
 
-    public static final String APPID = "";
+    public static final String APPID = "wx1d2e0320adaa9645";
 
-    public static final String APP_SECRET = "";
+    public static final String APP_SECRET = "42e94e7b7a5f75bb52af42af26305752";
 
     public static final String TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + APPID + "&secret=" + APP_SECRET;
 
